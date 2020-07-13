4dac11b988ea724d1cab821122f2cc503c68c94a
Bryan <b.gatay@efficacity.com>
2020-07-13 23:14:41 +0200

    Improve KPI BACK

--- server/src/controllers/dashboard.controller.js
+++ server/src/controllers/dashboard.controller.js
@@ -1,74 +1,118 @@
-
 import TransactionMongo, { emit } from "../models/mongo/Transaction";
 function mainDashboard(req, res) {
-    // res.json({ message: "🏦 Hello Pay Plussss+!!! 💰💰💰" })
-  
-    // Compte le nombre de transaction par marchant
-    
-    // Group by day - order amount
-    // TransactionMongo.aggregate([
-    //   { $match: {"merchant.id": 1  } },
-    //   { $group:
-    //      { 
-    //       _id : { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
-    //       total: { $sum: {"$toDouble": "$order_amount"} } } },
-    //   { $sort: { total: -1 } }
-    // ]).then((data) =>res.json(data))
-    
-    TransactionMongo.aggregate([
-      { $match: {"merchant.id": 1  } },
-      { $group:
-         { 
-          _id : { 
-            $dateToString: { format: "%d/%m/%Y", date: "$createdAt" } ,
-          },
-          order_amount: { $sum: {"$toDouble": "$order_amount"} },
-          number_product: { $sum: 1 }
+
+  TransactionMongo.aggregate([
+    {
+      $match: { "merchant.id": 1 },
+    },
+    {$project: {
+      operations: {
+        $sum:{
+          $filter:{
+            input:"$operations",
+            as:"s",
+            cond: { $eq: ["$$s.state", "DONE"] },
+          }
         }
+      }
+    }}
+
+  ]) .then((data) => {
+    console.log(data)
+  //  res.json(data);
+  })
+
+  TransactionMongo.aggregate([
+    {
+      $match: { "merchant.id": 1 },
+    },
+    // {
+    //   $redact: {
+    //     $cond: [
+    //       {
+    //         $eq: [
+    //           {
+    //             $let: {
+    //               vars: {
+    //                 item: { $arrayElemAt: ["$operations", -1] },
+    //               },
+    //               in: "$$item.type",
+    //             },
+    //           },
+    //           "CAPTURE",
+    //         ],
+    //       },
+    //       "$$KEEP",
+    //       "$$PRUNE",
+    //     ],
+    //   },
+    // },
+    {
+      $group: {
+        _id: {
+          $dateToString: { format: "%d/%m/%Y", date: "$createdAt" },
+        },
+        total_price_transaction: { $sum: { $toDouble: "$order_amount" } },
+        total_product: { 
+          $sum: { $sum: "$products.qte" } 
       },
-          
-      { $sort: { total: -1 } }
-    ]).then((data) => {
+      total_price_transaction_refund: { 
+        $sum: { $sum: "$operations.refund_amount" } 
+    },
+        average_price_by_transaction: { $avg: { $toDouble: "$order_amount" } },
+        number_transaction: { $sum: 1 },
+      },
+    },
+
+    { $sort: { total: -1 } },
+  ])
+    .then((data) => {
       const areaSats = {
         transactionPerformed: [],
-        dates: []
+        dates: [],
       };
-      Object.keys(data).forEach(element => {
-        areaSats.transactionPerformed.push(data[element]["_id"])
-        areaSats.dates.push(data[element]["order_amount"])
-       
+      Object.keys(data).forEach((element) => {
+        areaSats.transactionPerformed.push(data[element]["_id"]);
+        areaSats.dates.push(data[element]["total_price_transaction"]);
       });
-      console.log(areaSats)
-      res.json(data)
+      console.log(areaSats);
+      // res.json(data);
+    })
+    .catch((err) => console.log(err));
 
-    } )
 
+  TransactionMongo.aggregate([
+    { $match: { "merchant.id": 1 } },
+    { $group: { _id: "$item", total: { $sum: 1 } } },
 
+    // { $sort: { total: -1 } }
+  ]);
+  // .then((data) => res.json(data))
+  // .catch((err) => res.json(err))
 
-    
-    TransactionMongo.aggregate([
-        { $match: { "merchant.id": 1 } },
-        { $group: { _id: "$item", total: { $sum: 1 } } },
-        
-        // { $sort: { total: -1 } }
-      ])
-            // .then((data) => res.json(data))
-            // .catch((err) => res.json(err)) 
-
-    // get REFUNDED
-    TransactionMongo.aggregate([
-      {
-         $project: {
-            operations: {
-               $filter: {
-                  input: "$operations",
-                  as: "item",
-                  cond: { $eq: [ "$$item.type", "REFUNDED" ] }
-               }
-            },
-         }
+  // get REFUNDED
+  TransactionMongo.aggregate([
+    {
+      $match: { "merchant.id": 1 },
+    },
+    {
+      
+      $project: {
+        count: {
+          $size:{
+          $filter: {
+            input: "$operations",
+            as: "item",
+            cond: { $eq: ["$$item.state", "CANCELED"] },
+          },
+        },
       }
-   ]).then((data) => console.log("refunded"))
+      },
+    },
 
-  }
-export {mainDashboard}
+  ]).then((data) => {
+    console.log(" COUNTING => ", data)
+   res.json(data);
+  })
+}
+export { mainDashboard };
